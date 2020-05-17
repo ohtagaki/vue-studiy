2f7bed1e41c5c5235d46dace1b2011deeb5540cc
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-18 00:18:45 +0900

    第2回目講義終了

--- Second/study/src/App.vue
+++ Second/study/src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <transition mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -25,4 +23,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.v-enter-active, .v-leave-active {
+  transition: opacity 1.0s;
+}
+.v-enter, .v-leave-to {
+  opacity: 0;
+}
 </style>
+
---
+++ Second/study/src/components/Welcome.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <p>Welcome to Vue SPA!</p>
+        <router-link to="/">Go Back</router-link>
+    </div>
+</template>
